14b37b853389c546dd30188a2625aad909292f27
stif1550 <nibidocall@gmail.com>
2022-12-17 14:50:33 +0300

    fix

--- lab5/details_cmp.c
+++ lab5/details_cmp.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 
-int cmp_func_name_asс(const void *a, const void *b) {
+int cmp_func_name_asc(const void *a, const void *b) {
     detail *d1 = (detail *) a;
     detail *d2 = (detail *) b;
 
@@ -19,7 +19,7 @@ int cmp_func_name_desc(const void *a, const void *b) {
     return -strcmp(d1->name, d2->name);
 }
 
-int cmp_func_id_asс(const void *a, const void *b) {
+int cmp_func_id_asc(const void *a, const void *b) {
     detail *d1 = (detail *) a;
     detail *d2 = (detail *) b;
 
@@ -33,7 +33,7 @@ int cmp_func_id_desc(const void *a, const void *b) {
     return -strcmp(d1->id, d2->id);
 }
 
-int cmp_func_count_asс(const void *a, const void *b) {
+int cmp_func_count_asc(const void *a, const void *b) {
     detail *d1 = (detail *) a;
     detail *d2 = (detail *) b;
 
@@ -51,15 +51,15 @@ comp_func get_comp_func(int comparison, int sort_desc) {
     switch (comparison) {
         case SORT_NAME:
             if (!sort_desc)
-                return cmp_func_name_asс;
+                return cmp_func_name_asc;
             return cmp_func_name_desc;
         case SORT_ID:
             if (!sort_desc)
-            return cmp_func_id_asс;
+            return cmp_func_id_asc;
             return cmp_func_id_desc;
         case SORT_COUNT:
             if (!sort_desc)
-            return cmp_func_count_asс;
+            return cmp_func_count_asc;
             return cmp_func_count_desc;
     }
     fprintf(stderr,"illegal comparison number %d\n",comparison);
